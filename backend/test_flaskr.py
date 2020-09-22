09923cc007a65320ec354327e1213034b09ac366
Otavio Ho Aragoni <otavio.aragoni@gmail.com>
2020-09-22 11:18:18 -0300

    add tests

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://postgres:secret@{}/{}".format('localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,7 +33,33 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_cat(self):
+        response = self.client().get('/categories')
+        self.assertEqual(response.status_code, 200)
 
+    def test_questions(self):
+        response = self.client().get('/questions')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.data)['total_questions'], 19)
+
+    def test_delete(self):
+        response = self.client().delete('/questions/23')
+        self.assertEqual(json.loads(response.data)['success'], True)
+
+    def test_post(self):
+        new_question = { 
+          'question': "Test question",
+          'answer': 'Testing',
+          'category': 3,
+          'difficulty': 3
+        }
+
+        response = self.client().post('/questions', json=new_question)
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_fail(self):
+        response = self.client().post('/questions', json={'test': 'fail'})
+        self.assertEqual(response.status_code, 422)
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
